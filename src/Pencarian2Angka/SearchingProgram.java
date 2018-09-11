b41da75c76fa8db7aa61d37204db37d7d68e80dc
Acer <Acer@Acer-PC>
2018-09-11 18:07:35 +0700

    Finish

--- src/Pencarian2Angka/SearchingProgram.java
+++ src/Pencarian2Angka/SearchingProgram.java
@@ -7,69 +7,71 @@ public class SearchingProgram {
 
     public static void main(String[] args) {
 
-        do{
-        int jlh_data = Integer.parseInt(JOptionPane.showInputDialog("Masukkan jumlah data"));
-        System.out.print("Jumlah Data : " + jlh_data);
-        System.out.println("\n========================================================");
+        int ulang;
 
-        int[] data = new int[jlh_data];
+        do {
+            int jlh_data = Integer.parseInt(JOptionPane.showInputDialog("Masukkan jumlah data"));
+            System.out.print("Jumlah Data : " + jlh_data);
+            System.out.println("\n========================================================");
 
-        System.out.println("\nPENGISIAN DATA (SECARA RANDOM)");
+            int[] data = new int[jlh_data];
 
-        for (int a = 0; a < jlh_data; a++) {
-            data[a] = new Random().nextInt(201);
-        }
+            System.out.println("\nPENGISIAN DATA (SECARA RANDOM)");
 
-        System.out.println("\nTampilkan Data");
-        for (int a = 0; a < jlh_data; a++) {
-            System.out.println("Data ke-" + (a + 1) + " : " + data[a]);
-        }
-
-        System.out.println("========================================================");
-        
-        int cari = Integer.parseInt(JOptionPane.showInputDialog("Data yang dicari \nPertama : "));
-        System.out.println("\nInput Data yang dicari :\nPertama : " + cari);
-
-        int cari1 = Integer.parseInt(JOptionPane.showInputDialog("Kedua : "));
-        System.out.println("Kedua : " + cari1);
-
-        boolean temu = false;
-        boolean temu1 = false;
-        int urutan = 0;
-        int urutan1 = 0;
-        System.out.println("========================================================");
-        
-        for (int a = 0; a < jlh_data; a++) {
-            if (data[a] == cari) {
-                urutan = a + 1;
-                temu = true;
-                break;
+            for (int a = 0; a < jlh_data; a++) {
+                data[a] = new Random().nextInt(201);
             }
-        }
 
-        for (int a = 0; a < jlh_data; a++) {
-            if (data[a] == cari1) {
-                urutan1 = a + 1;
-                temu1 = true;
-                break;
+            System.out.println("\nTampilkan Data");
+            for (int a = 0; a < jlh_data; a++) {
+                System.out.println("Data ke-" + (a + 1) + " : " + data[a]);
             }
-        }
 
-        if (temu == true) {
-            System.out.println("\nData " + cari + " ditemukan pada urutan ke-" + (urutan));
-        } else {
-            System.out.println("\nData " + cari + " tidak ditemukan");
-        }
-        
-        if (temu1 == true) {
-            System.out.println("\nData " + cari1 + " ditemukan pada urutan ke-" + (urutan1));
-        } else {
-            System.out.println("\nData " + cari1 + " tidak ditemukan");
-        }
-        int ulang = Integer.parseInt(JOptionPane.showInputDialog("Ulangi proses?\n"));
-        System.out.println("\nInput Data yang dicari :\nPertama : " + cari);
-        }while
-        System.out.println("\n========================================================");
+            System.out.println("========================================================");
+
+            int cari = Integer.parseInt(JOptionPane.showInputDialog("Data yang dicari \nPertama : "));
+            System.out.println("\nInput Data yang dicari :\nPertama : " + cari);
+
+            int cari1 = Integer.parseInt(JOptionPane.showInputDialog("Kedua : "));
+            System.out.println("Kedua : " + cari1);
+
+            boolean temu = false;
+            boolean temu1 = false;
+            int urutan = 0;
+            int urutan1 = 0;
+            System.out.println("========================================================");
+
+            for (int a = 0; a < jlh_data; a++) {
+                if (data[a] == cari) {
+                    urutan = a + 1;
+                    temu = true;
+                    break;
+                }
+            }
+
+            for (int a = 0; a < jlh_data; a++) {
+                if (data[a] == cari1) {
+                    urutan1 = a + 1;
+                    temu1 = true;
+                    break;
+                }
+            }
+
+            if (temu == true) {
+                System.out.println("\nData " + cari + " ditemukan pada urutan ke-" + (urutan));
+            } else {
+                System.out.println("\nData " + cari + " tidak ditemukan");
+            }
+
+            if (temu1 == true) {
+                System.out.println("\nData " + cari1 + " ditemukan pada urutan ke-" + (urutan1));
+            } else {
+                System.out.println("\nData " + cari1 + " tidak ditemukan");
+            }
+            ulang = Integer.parseInt(JOptionPane.showInputDialog("Ulangi proses?\n1 untuk YES\n2 untuk NO"));
+            System.out.println("\n========================================================");
+        } while (ulang == 1);
+        System.out.println("Program By :5");
         System.out.println("Hammam Al-Hakim/XRPL6/27");
     }
 }
